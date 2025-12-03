ed8d835c2421e7f417eba32cb226e4ed8902ca09
scontreraslopez <s.contreraslopez@edu.gva.es>
2025-12-03 15:42:56 +0100

    avanzando

--- app/src/main/java/net/iessochoa/sergiocontreras/mercadonadb/ui/screens/ShopScreen.kt
+++ app/src/main/java/net/iessochoa/sergiocontreras/mercadonadb/ui/screens/ShopScreen.kt
@@ -1,9 +1,20 @@
 package net.iessochoa.sergiocontreras.mercadonadb.ui.screens
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import net.iessochoa.sergiocontreras.mercadonadb.model.ShoppingStrategy
+import net.iessochoa.sergiocontreras.mercadonadb.ui.components.DynamicSelectTextField
+import net.iessochoa.sergiocontreras.mercadonadb.ui.components.Product
+import net.iessochoa.sergiocontreras.mercadonadb.ui.components.ResultCard
+import net.iessochoa.sergiocontreras.mercadonadb.ui.components.SupermarketBanner
 
 /**
  * Project: MercadonaDB
@@ -15,8 +26,41 @@ import androidx.compose.ui.Modifier
 
 @Composable
 fun ShopScreen(modifier: Modifier = Modifier) {
-    Column {
-        Text("TODO")
+    Column (modifier = modifier, horizontalAlignment = Alignment.CenterHorizontally) {
+        var selectedStrategy = ShoppingStrategy.CHEAPEST
+        val sampleProduct = Product(
+            category = "Fruta",
+            name = "Sandía",
+            price = 3.75,
+            referencePrice = 0.55,
+            referenceUnit = "kg"
+        )
+
+        SupermarketBanner()
+        DynamicSelectTextField(
+            selectedValue = selectedStrategy.toString(),
+            options = ShoppingStrategy.entries.map {
+                it.toString()
+            },
+            label = "Estrategia de Compra",
+            onValueChangedEvent = {}
+        )
+        Spacer(modifier.height(16.dp))
+
+        Button(onClick = { /*TODO*/ }) {
+            Text("Voy a tener suerte")
+        }
+        ResultCard(
+            product = sampleProduct,
+            strategy = ShoppingStrategy.CHEAPEST
+        )
+
     }
 
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PreviewShopScreen() {
+    ShopScreen()
 }
