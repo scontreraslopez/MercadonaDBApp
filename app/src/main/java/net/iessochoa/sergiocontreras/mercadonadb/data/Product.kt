b0b5c025b6578666ca2ab7def27021aab6165088
scontreraslopez <s.contreraslopez@edu.gva.es>
2025-12-19 13:23:49 +0100

    wip entity created

--- app/src/main/java/net/iessochoa/sergiocontreras/mercadonadb/data/Product.kt
+++ app/src/main/java/net/iessochoa/sergiocontreras/mercadonadb/data/Product.kt
@@ -5,27 +5,21 @@ import androidx.room.Entity
 import androidx.room.PrimaryKey
 import java.time.LocalDateTime
 
-@Entity
-data class Product(
-    // 1. ID Interno vs ID del CSV
-    @PrimaryKey(autoGenerate = true)
-    @ColumnInfo(name = "id")
+@Entity(tableName = "products")
+data class Product (
+    // 1. ID Interno vs ID del CSV. Generamos el nuestro propio
+    @PrimaryKey (autoGenerate = true)
+    @ColumnInfo (name = "id")
     val id: Long = 0,
 
-    @PrimaryKey(autoGenerate = true) val id: Int,
-    @ColumnInfo(name = "supermarket") val supermarket: String,
-    @ColumnInfo(name = "category") val category: String,
-    @ColumnInfo (name = "name") val name: String,
-    @ColumnInfo (name = "price") val productPrice: Double,
+    @ColumnInfo (name = "externalId") val externalId: String?,
+    @ColumnInfo (name = "supermarket") val supermarket: String,
+    @ColumnInfo (name = "category") val category: String,
+    @ColumnInfo (name = "product_name") val name: String, //Evitar palabras reservadas que nos la lían
+    @ColumnInfo (name = "price") val price: Double, //Puede dar problemas de precisión pero para esto sirve
     @ColumnInfo (name = "reference_price") val referencePrice: Double,
     @ColumnInfo (name = "reference_unit") val referenceUnit: String,
-    @ColumnInfo (name = "insert_date") val insertDate: LocalDateTime
+    @ColumnInfo (name = "insert_date") val recordDate: LocalDateTime
 )
 
-    val reference_price
-    val reference_uit
-    val insert_date
-
-
-    supermarket, category, name, price, reference_price, reference_unit, insert_date
 
