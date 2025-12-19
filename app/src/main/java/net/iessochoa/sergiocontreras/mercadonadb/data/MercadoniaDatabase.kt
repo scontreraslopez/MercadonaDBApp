a0a1ff85ec7e9e6465b50403d0fbbdbf9d37bd03
scontreraslopez <s.contreraslopez@edu.gva.es>
2025-12-19 17:22:25 +0100

    database created

--- app/src/main/java/net/iessochoa/sergiocontreras/mercadonadb/data/MercadoniaDatabase.kt
+++ app/src/main/java/net/iessochoa/sergiocontreras/mercadonadb/data/MercadoniaDatabase.kt
@@ -1,6 +1,8 @@
 package net.iessochoa.sergiocontreras.mercadonadb.data
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
 
 /**
@@ -13,5 +15,30 @@ import androidx.room.RoomDatabase
 
 @Database(entities=[Product::class], version = 1, exportSchema = false) //Puede ser interesante hacerlo true
 abstract class MercadoniaDatabase: RoomDatabase() {
+
+    abstract fun productDao(): ProductDao
+
+    companion object {
+        const val DATABASE_NAME = "mercadonia.db"
+
+        @Volatile
+        private var Instance: MercadoniaDatabase? = null
+
+        fun getDatabase(context: Context) : MercadoniaDatabase {
+            //if the Instance is not null, return it, otherwise create a new database instance.
+            return Instance ?: synchronized(this) {
+                Room.databaseBuilder(context, MercadoniaDatabase::class.java, DATABASE_NAME)
+                    /**
+                     * Setting this option in your app's database builder means that Room
+                     * permanently deletes all data from the tables in your database when it
+                     * attempts to perform a migration with no defined migration path.
+                     */
+                    .fallbackToDestructiveMigration(true)
+                    .build()
+                    .also { Instance = it }
+            }
+        }
+
+    }
 
 }
